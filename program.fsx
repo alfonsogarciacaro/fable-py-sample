95fca831591e167be4f604931a476ef45ea14855
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2022-06-23 16:27:14 +0900

    Some use with Pandas

--- program.fsx
+++ program.fsx
@@ -1,6 +1,21 @@
 ﻿#r "nuget:Fable.Python"
 
+open Fable.Core
+open Fable.Core.PyInterop
 open Fable.Python.Builtins
+
+module Pandas =
+    type DataFrame =
+        [<Emit("$0[$1]")>]
+        abstract loc: label: obj -> 'T
+
+    type IPandas =
+        abstract read_csv: string -> DataFrame
+
+    [<ImportAll("pandas")>]
+    let pandas: IPandas = nativeOnly
+
+open Pandas
 
 [<EntryPoint>]
 let main argv =
@@ -10,5 +25,14 @@ let main argv =
     // Open file with builtin `open`
     use file = builtins.``open``(StringPath "data.txt")
     file.read() |> printfn "File contents: %s"
+
+    // Open file with Pandas. We don't have bindings yet
+    // but you can use dynamic programming
+    let df = pandas.read_csv("numbers.csv")
+    builtins.print(df)
+    let columnA: int[] = df.loc("A")
+    columnA |> Array.map (fun i -> i + 4) |> Array.sum |> printfn "%i"
+
+    // pandas?plotting?andrews_curves(df, "B")
 
     0
