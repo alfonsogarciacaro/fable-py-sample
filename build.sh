fe01c51f18f89745253c2e5795f1365ffd98a59e
Dag Brattli <dag@brattli.net>
2022-07-27 15:38:55 +0200

    fix: update for -alpha-017

--- build.sh
+++ build.sh
@@ -1,7 +1,2 @@
 dotnet tool restore
-
-# Build once and install the Python packages
-dotnet fable --lang py
-pip install -r fable_modules/requirements.txt
-
 dotnet fable watch --lang py --runWatch program.py "$@"
